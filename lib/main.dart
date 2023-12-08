f4d0ee0c3293b14b17f8c731d931436aaeec139b
AhmadKusumahDEV <fatursofyan.fs@gmail.com>
2023-12-08 16:12:58 +0700

    tamat

--- lib/main.dart
+++ lib/main.dart
@@ -19,43 +19,43 @@ import 'package:flutter/material.dart';
 // import './tugas7/beranda.dart';
 
 // tugas 8
-import './tugas8/beranda.dart';
+// import './tugas8/beranda.dart';
 
 // import './kesebelas/services/poli_services.dart';
 
 // p 11
-// import './kesebelas/helpers/user_info.dart';
-// import './kesebelas/beranda.dart';
-// import './kesebelas/login.dart';
+import './kesebelas/helpers/user_info.dart';
+import './kesebelas/beranda.dart';
+import './kesebelas/login.dart';
 
 // p 12
 // import './keduabelas/helpers/user_info.dart';
 // import './keduabelas/beranda.dart';
 // import './keduabelas/login.dart';
-import './keduabelas/poli_page.dart';
+// import './keduabelas/poli_page.dart';
 
 // P 13
 // import './ketigabelas/poli_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   var oken = await Userinfo().getToken();
-//   runApp(MaterialApp(
-//     debugShowCheckedModeBanner: false,
-//     home: oken == null ? const Login() : const Beranda(),
-//   ));
+// void main() {
+//   runApp(const MyApp());
 // }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: Beranda());
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var oken = await Userinfo().getToken();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: oken == null ? const Login() : const Beranda(),
+  ));
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//         debugShowCheckedModeBanner: false, home: Beranda());
+//   }
+// }
